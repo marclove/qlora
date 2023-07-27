e973cb33420b1789433b80ba53bfa7d3c7dd5012
Marc Love <marclove@users.noreply.github.com>
2023-07-27 11:52:50 -0400

    Don’t think I merged correctly

--- merge.py
+++ merge.py
@@ -1,7 +1,8 @@
+import torch
 import os
 from os.path import exists, join, isdir
 from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
-from peft import PeftModel
+from peft import PeftModel, AutoPeftModelForCausalLM
 
 
 def get_last_checkpoint(checkpoint_dir):
@@ -20,19 +21,31 @@ def get_last_checkpoint(checkpoint_dir):
 
 
 def merge():
+    name = "llama-2-7b-chat-hf-functions-v1"
     output_dir = "/content/drive/MyDrive/llama-2-chat-functions-7b"
     checkpoint_dir, _completed_training = get_last_checkpoint(output_dir)
-    config = AutoConfig.from_pretrained(join(checkpoint_dir, 'adapter_model', 'adapter_config.json'))
+    adapter_dir = join(checkpoint_dir, 'adapter_model')
+    model = AutoPeftModelForCausalLM.from_pretrained(adapter_dir, device_map="auto", torch_dtype=torch.bfloat16)
+    model = model.merge_and_unload()
+    output_merged_dir = join(output_dir, "merged")
+    os.makedirs(output_merged_dir, exist_ok=True)
+    model.save_pretrained(output_merged_dir, safe_serialization=True)
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-    base_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map="auto")
-    base_model.config = config
-    model = PeftModel.from_pretrained(base_model, join(checkpoint_dir, 'adapter_model'))
-    model = model.base_model.model
-    name = "llama-2-7b-chat-hf-functions-v1"
-    # model.save_pretrained(join(output_dir, "final"))
-    # tokenizer.save_pretrained(join(output_dir, "final"))
+    tokenizer.save_pretrained(output_merged_dir)
     model.push_to_hub(name, use_temp_dir=True)
     tokenizer.push_to_hub(name, use_temp_dir=True)
+
+    # config = AutoConfig.from_pretrained(join(checkpoint_dir, 'adapter_model', 'adapter_config.json'))
+    # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+    # base_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map="auto")
+    # base_model.config = config
+    # model = PeftModel.from_pretrained(base_model, join(checkpoint_dir, 'adapter_model'))
+    # model = model.base_model.model
+    # name = "llama-2-7b-chat-hf-functions-v1"
+    # model.save_pretrained(join(output_dir, "final"))
+    # tokenizer.save_pretrained(join(output_dir, "final"))
+    # model.push_to_hub(name, use_temp_dir=True)
+    # tokenizer.push_to_hub(name, use_temp_dir=True)
 
 if __name__ == "__main__":
     merge()
